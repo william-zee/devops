7ef95d209f8353d8beba0fa753fa1626cb363044
William Zee <william.zee@example.com>
2025-12-09 11:00:30 +0100

    Ajout du code Node.js du Labo 1

---
+++ ch1/sample-app/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.end('Hello, World!\n');
+});
+const port = process.env.PORT || 8080;
+server.listen(port,() => {
+console.log(`Listening on port ${port}`);
+});
